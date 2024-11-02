2cda9cc273ad6d562b26c5cb8e4ea5410cc267bf
Matias <matias.telo000@gmail.com>
2024-11-02 18:25:43 -0300

    Finalizacion de proyecto

--- src/main/java/com/example/AnalisisADN/Services/HumanoServiceImpl.java
+++ src/main/java/com/example/AnalisisADN/Services/HumanoServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public abstract class HumanoServiceImpl extends BaseServiceImpl<Humano, Long> implements HumanoService{
+public class HumanoServiceImpl extends BaseServiceImpl<Humano, Long> implements HumanoService{
 
     @Autowired
     private HumanoRepository humanoRepository;
